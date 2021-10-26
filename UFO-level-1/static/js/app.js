940d25287a3fb072b765496bd0e74085c5955386
michael-sorensen <86160279+michael-sorensen@users.noreply.github.com>
2021-10-26 10:32:56 -0600

    Final Files

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,89 @@
+/* EXAMPLE DATA 
+
+var data = [{
+  datetime: "1/1/2010",
+  city: "benton",
+  state: "ar",
+  country: "us",
+  shape: "circle",
+  durationMinutes: "5 mins.",
+  comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+},
+*/
+
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+var row = tbody.select("tr");
+
+// Step 3:  Use Object.entries to console.log each weather report value
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+  });
+});
+
+// Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+  });
+
+// Step 5: Use d3 to update each cell's text with
+// weather report values (weekday, date, high, low)
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Select button & form
+var filterButton = d3.select("button#filter-btn");
+var form = d3.select("#filters");
+
+// Event handlers 
+filterButton.on("click", runEnter);
+form.on("submit",preventSubmit);
+
+function preventSubmit() {
+  d3.event.preventDefault;
+};
+
+// Event handler | "runEnter" function
+function runEnter() {
+  var inputElement = d3.select("#datetime");
+  console.log(inputElement);
+
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+
+  var searchResults = tableData.filter(d => (d.datetime === inputValue));
+  console.log(searchResults)
+
+
+// Reuturn search results in table
+tbody.html("");
+searchResults.forEach(
+  function(ufoData) {
+      var row = tbody.append("tr");
+      var values = Object.values(ufoData);
+      values.forEach(
+          function(val){
+              row.append("td").text(val);
+          }
+      )
+  }
+)};
+
